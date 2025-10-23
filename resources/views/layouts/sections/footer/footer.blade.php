767438ec59bd277a876b01041faf2ee3e84a1494
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2025-10-23 09:58:26 -0500

    datos de presentacion/uis

--- resources/views/layouts/sections/footer/footer.blade.php
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -1,5 +1,5 @@
 @php
-$containerFooter = $configData['contentLayout'] === 'compact' ? 'container-xxl' : 'container-fluid';
+    $containerFooter = $configData['contentLayout'] === 'compact' ? 'container-xxl' : 'container-fluid';
 @endphp
 
 <!-- Footer -->
@@ -9,16 +9,24 @@ $containerFooter = $configData['contentLayout'] === 'compact' ? 'container-xxl' 
             <div class="mb-2 mb-md-0">
                 &#169;
                 <script>
-                document.write(new Date().getFullYear());
+                    document.write(new Date().getFullYear());
                 </script>
-                , made with ❤️ by <a href="{{ !empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '' }}" target="_blank" class="footer-link fw-medium">{{ !empty(config('variables.creatorName')) ? config('variables.creatorName') : '' }}</a>
+                , hecho ❤️ por <a
+                    href="{{ !empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '' }}"
+                    target="_blank"
+                    class="footer-link fw-medium">{{ !empty(config('variables.creatorName')) ? config('variables.creatorName') : '' }}</a>
             </div>
             <div class="d-none d-lg-inline-block">
-                <a href="{{ config('variables.adminTemplates') ? config('variables.adminTemplates') : '#' }}" class="footer-link me-4" target="_blank">Admin Templates</a>
-                <a href="{{ config('variables.licenseUrl') ? config('variables.licenseUrl') : '#' }}" class="footer-link me-4" target="_blank">License</a>
-                <a href="{{ config('variables.bootstrapDashboard') ? config('variables.bootstrapDashboard') : '#' }}" target="_blank" class="footer-link me-4">Bootstrap Dashboard</a>
-                <a href="{{ config('variables.documentation') ? config('variables.documentation') . '/laravel-introduction.html' : '#' }}" target="_blank" class="footer-link me-4">Documentation</a>
-                <a href="{{ config('variables.support') ? config('variables.support') : '#' }}" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
+                <a href="{{ config('variables.adminTemplates') ? config('variables.adminTemplates') : '#' }}"
+                    class="footer-link me-4" target="_blank">Admin Templates</a>
+                <a href="{{ config('variables.licenseUrl') ? config('variables.licenseUrl') : '#' }}"
+                    class="footer-link me-4" target="_blank">License</a>
+                <a href="{{ config('variables.bootstrapDashboard') ? config('variables.bootstrapDashboard') : '#' }}"
+                    target="_blank" class="footer-link me-4">Bootstrap Dashboard</a>
+                <a href="{{ config('variables.documentation') ? config('variables.documentation') . '/laravel-introduction.html' : '#' }}"
+                    target="_blank" class="footer-link me-4">Documentation</a>
+                <a href="{{ config('variables.support') ? config('variables.support') : '#' }}" target="_blank"
+                    class="footer-link d-none d-sm-inline-block">Support</a>
             </div>
         </div>
     </div>
