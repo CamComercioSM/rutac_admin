fd6c59d49999351a882e3d19cfba4a531cb60a7d
Brayan Rene Carbono Carbono <brayan.carbono@arandasoft.com>
2025-10-24 20:34:42 -0500

    UserTrait

--- app/Models/Capsula.php
+++ app/Models/Capsula.php
@@ -7,10 +7,11 @@ use App\Models\TablasReferencias\Etapa;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Traits\UserTrait;
 
 class Capsula extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, UserTrait;
 
     protected $table = 'capsulas';
     protected $primaryKey = 'capsula_id';
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use App\Models\Empresarios\UnidadProductiva;
+use App\Models\Traits\UserTrait;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, CanResetPassword;
+    use HasFactory, Notifiable, CanResetPassword, UserTrait;
 
     protected $table = 'users';
 
