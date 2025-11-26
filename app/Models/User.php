c3885481771076f240a8984b38c8ac4ea82b9ac3
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2025-11-26 12:35:01 -0500

    agragado el campo email_:cargo

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
         'lastname',
         'position',
         'email',
+        'email_cargo',
         'password',
         'google_id',
         'email_verified_at',
