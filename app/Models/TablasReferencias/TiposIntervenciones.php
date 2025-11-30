f1940c87b1346954277f67c59dba7ca697dfd5cb
Brayan Rene Carbono Carbono <brayan.carbono@arandasoft.com>
2025-11-30 14:12:34 -0500

    Ajuste tabla

--- app/Models/TablasReferencias/TiposIntervenciones.php
+++ app/Models/TablasReferencias/TiposIntervenciones.php
@@ -12,4 +12,8 @@ class TiposIntervenciones extends Model
     protected $fillable = [
         'nombre'
     ];
+    
+    const CREATED_AT = 'fecha_creacion';
+    const UPDATED_AT = 'fecha_actualizacion';
+    const DELETED_AT = 'fecha_eliminacion';
 }
